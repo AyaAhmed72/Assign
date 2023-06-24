7d5ebf323e08f01c991517aaedb6843f79992bf2
AyaAhmed72 <130875426+AyaAhmed72@users.noreply.github.com>
2023-06-24 22:45:47 +0300

    function1.h

--- function1.h
+++ function1.h
@@ -1,4 +1,7 @@
 #include<iostream>
 using namespace std;
-bool isSafe(int row, int col, int N);
-bool solveNQueens(int col, int N);
+#define N 8
+void printBoard(int board[N][N]);
+bool isValid(int board[N][N], int row, int col);
+bool solveNQueen(int board[N][N], int col);
+bool checkSolution();
